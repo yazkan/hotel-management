fb7e9ca8b17d818e852db34a06a55be7818e2ac4
Nazmi <nazmiyazkan@gmail.com>
2023-12-01 20:11:11 +0300

    create and insert room routes added

--- routes/roomRouter.js
+++ routes/roomRouter.js
@@ -1,15 +1,31 @@
 import { Router } from "express";
-import { getRoom, getAllRooms } from "../controllers/roomController.js";
+import {
+  getRoom,
+  getAllRooms,
+  deleteRoom,
+  createRoom,
+} from "../controllers/roomController.js";
+
 const app = Router();
 
 app.get("/roomAll", (req, res) => {
-  console.log("Log: Get request '/roomAll'");
+  console.log("Log: Get request /roomAll");
   getAllRooms(req, res);
 });
 
 app.get("/room/:id", (req, res) => {
-  console.log("Log: Get request room id: " + req.params.id);
+  console.log("Log: Get request /room/:id=" + req.params.id);
   getRoom(req, res);
+});
+
+app.delete("/room/:id", (req, res) => {
+  console.log("Log: Delete request /room/:id=" + req.params.id);
+  deleteRoom(req, res);
+});
+
+app.post("/room", (req, res) => {
+  console.log("Log: Post request /room");
+  createRoom(req, res);
 });
 
 export default app;
