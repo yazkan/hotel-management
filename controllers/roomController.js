cd82145fae36376799bb0003b3939ebe8d815a91
Nazmi <nazmiyazkan@gmail.com>
2023-12-01 20:10:41 +0300

    delete and insert room added

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -22,3 +22,31 @@ export const getAllRooms = (req, res) => {
     res.status(200).json(result);
   });
 };
+
+export const deleteRoom = (req, res) => {
+  connection.query(
+    "Delete FROM users WHERE userID=" + req.params.id,
+    function (err, result, fields) {
+      if (err) throw err; // TODO: handle error
+      res.status(200).json(result);
+    }
+  );
+};
+
+export const createRoom = (req, res) => {
+  connection.query(
+    "INSERT INTO users (username,password,name,surname) VALUES ('" +
+      req.body.username +
+      "','" +
+      req.body.password +
+      "','" +
+      req.body.name +
+      "','" +
+      req.body.surname +
+      "')",
+    function (err, result, fields) {
+      if (err) throw err; // TODO: handle error
+      res.status(200).json({ message: "User created." });
+    }
+  );
+};
