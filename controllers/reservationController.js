03473fbe1a6d793a00ac8919b35f8476ab7005c4
Nazmi <nazmiyazkan@gmail.com>
2023-12-02 22:08:55 +0300

    reservation requests handled

---
+++ controllers/reservationController.js
@@ -0,0 +1,72 @@
+import connection from "../dbConnect.js";
+
+export const getAllReservation = (req, res) => {
+  connection.query(
+    "SELECT * FROM reservations",
+    function (err, result, fields) {
+      if (err) throw err; // TODO: handle error
+      res.status(200).json(result);
+    }
+  );
+};
+
+export const createReservation = (req, res) => {
+  connection.query(
+    "INSERT INTO reservations (customer_id,room_no) VALUES ('" +
+      req.body.customer_id +
+      "','" +
+      req.body.room_no +
+      "')",
+    function (err, result, fields) {
+      if (err) throw err; // TODO: handle error
+      res.status(200).json({ message: "Reservation created." });
+    }
+  );
+};
+
+export const getReservation = (req, res) => {
+  if (!isNaN(req.params.id)) {
+    connection.query(
+      "SELECT * FROM reservations WHERE reservation_id=" + req.params.id,
+      function (err, result, fields) {
+        if (err) throw err; // TODO: handle error
+        res.status(200).json(result);
+      }
+    );
+  } else {
+    res
+      .status(400)
+      .json({ message: "Hatalı giriş! Geçerli bir reservation id giriniz!" });
+  }
+};
+
+export const deleteReservation = (req, res) => {
+  if (!isNaN(req.params.id)) {
+    connection.query(
+      "Delete FROM reservations WHERE reservation_id=" + req.params.id,
+      function (err, result, fields) {
+        if (err) throw err; // TODO: handle error
+        res.status(200).json(result);
+      }
+    );
+  } else {
+    res
+      .status(400)
+      .json({ message: "Hatalı giriş! Geçerli bir reservation id giriniz!" });
+  }
+};
+
+export const updateRoom = (req, res) => {
+  connection.query(
+    "UPDATE reservations SET customer_id='" +
+      req.body.customer_id +
+      "', room_no='" +
+      req.body.room_no +
+      "' WHERE reservation_id=" +
+      req.params.id,
+    function (err, result, fields) {
+      if (err) throw err; // TODO: handle error
+      res.status(200).json({ message: "Reservation updated." });
+    }
+  );
+};
--- routes/reservationRouter.js
+++ routes/reservationRouter.js
@@ -1,9 +1,37 @@
 import { Router } from "express";
+import {
+  getAllReservation,
+  getReservation,
+  createReservation,
+  deleteReservation,
+  updateReservation,
+} from "../controllers/reservationController.js";
+
 const app = Router();
 
 app.get("/reservation", (req, res) => {
-  console.log("Log: Get request '/reservation'");
-  res.status(200).send("reservation");
+  console.log("Log: Get request /reservation");
+  getAllReservation(req, res);
+});
+
+app.get("/reservation/:id", (req, res) => {
+  console.log("Log: Get request /reservation/:id=" + req.params.id);
+  getReservation(req, res);
+});
+
+app.delete("/reservation/:id", (req, res) => {
+  console.log("Log: Delete request /reservation/:id=" + req.params.id);
+  deleteReservation(req, res);
+});
+
+app.post("/reservation", (req, res) => {
+  console.log("Log: Post request /reservation");
+  createReservation(req, res);
+});
+
+app.post("/reservation/:id", (req, res) => {
+  console.log("Log: Post request /reservation/:id=" + req.params.id);
+  updateReservation(req, res);
 });
 
 export default app;
