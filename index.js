72330e26ac2476392b3fe9b61f5ac1b878dd6a44
yazkan <nazmiyazkan@gmail.com>
2024-01-03 13:09:11 +0300

    port changed

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ app.get("/", (req, res) => {
   console.log("Log: Get request '/'");
 });
 
-app.listen(3000, () => console.log("Server is running on port: 3000"));
+app.listen(9999, () => console.log("Server is running on port: 5000"));
