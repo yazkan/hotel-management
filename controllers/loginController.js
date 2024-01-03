42a45c7f8ec0bc95a87c359f10471f4704f816e5
yazkan <nazmiyazkan@gmail.com>
2024-01-03 18:28:24 +0300

    login fixed

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,26 +2,30 @@ import connection from "../dbConnect.js";
 
 export const loginCustomer = (req, res) => {
   connection.query(
-    "SELECT * FROM customers WHERE username=" +
+    "SELECT * FROM customers WHERE customer_username=" +
       connection.escape(req.body.customer_username) +
-      " AND password=" +
+      " AND customer_password=" +
       connection.escape(req.body.customer_password),
     function (err, result, fields) {
       if (err) throw err; // TODO: handle error
-      res.status(200).json(result);
+      if(result.length !=1)
+        res.status(403).send("Customer couldn't find!");
+      res.status(200).json(result[0]);
     }
   );
 };
 
 export const loginEmployer = (req, res) => {
   connection.query(
-    "SELECT * FROM employees WHERE username=" +
+    "SELECT * FROM employees WHERE employee_username=" +
       connection.escape(req.body.employee_username) +
-      " AND password=" +
+      " AND employee_password=" +
       connection.escape(req.body.employee_password),
     function (err, result, fields) {
       if (err) throw err; // TODO: handle error
-      res.status(200).json(result);
+      if(result.length !=1)
+        res.status(403).send("Employer couldn't find!");
+      res.status(200).json(result[0]);
     }
   );
 };
