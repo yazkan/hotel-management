65c88284856506879ba6a38a34029de69fc96d30
yazkan <nazmiyazkan@gmail.com>
2024-01-03 13:11:28 +0300

    port

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ app.get("/", (req, res) => {
   console.log("Log: Get request '/'");
 });
 
-app.listen(9999, () => console.log("Server is running on port: 5000"));
+app.listen(9999, () => console.log("Server is running on port: 9999"));
